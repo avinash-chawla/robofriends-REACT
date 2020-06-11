8f8b50a7f43f4d87a3c58520ff2ad6bf1de70fe3
avinashchawla96 <avinashchawla96@gmail.com>
2020-06-11 12:35:12 +0530

    Updates

---
+++ precache-manifest.dbb7f544339cbf6a3425ee912fd5c7c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e1b10519e93eb61ded28ccd215ea09",
+    "url": "/robofriends-REACT/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends-REACT/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "da52c9c48922af252bdd",
+    "url": "/robofriends-REACT/static/css/main.6677c54d.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends-REACT/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-REACT/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da52c9c48922af252bdd",
+    "url": "/robofriends-REACT/static/js/main.ed180c1c.chunk.js"
+  },
+  {
+    "revision": "e582bcc5896969869773",
+    "url": "/robofriends-REACT/static/js/runtime-main.35918fdf.js"
+  }
+]);
